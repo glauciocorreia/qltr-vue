428837bcf924c8c1e40de0b0da4597659114e118
glauciocorreia <glauciocorreiaa@gmail.com>
2019-05-22 17:45:47 -0300

    First Commit + Set Components Base + Start Navbar code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
+    <Navbar></Navbar>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
+    Navbar,
     HelloWorld
   }
 }
 </script>
 
 <style lang="scss">
+
+@import 'assets/css/reset.css';
+
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+}
+
+li {
+  list-style: none;
+}
+
+a {
+    text-decoration: none;
+}
+
+.d-flex {
+  display: flex;
+}
+.justify-content-between {
+  justify-content: space-between;
+}
+.align-items-center {
+  align-items: center;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,49 @@
+<template>
+  <header id="#header">
+    <nav id="nav-menu" class="d-flex justify-content-between align-items-center">
+      <a class="nav-logo" href="#">Looper</a>
+      <ul class="nav-list d-flex justify-content-between align-items-center">
+        <li v-for="item in navLinks" :key="item.title">
+          <a :href="`${ item.href }`">{{ item.title }}</a>
+        </li>
+      </ul>
+      <a href="#">Looper</a>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      navLinks: [
+        { href: "#", title: "Home" },
+        { href: "#", title: "Services" },
+        { href: "#", title: "Work" },
+        { href: "#", title: "About" },
+        { href: "#", title: "(01) 666 - 693 - 456" }
+      ]
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+
+
+
+
+header {
+    padding: 3% 4% 5% 4%;
+
+}
+nav {
+  a.nav-logo {
+    font-weight: 900;
+	font-size: 24px;
+	line-height: 43px;
+	letter-spacing: 2px;
+	color: $primary-color;
+  }
+}
+</style>
